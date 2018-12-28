19fa33a86dd372e927988ab581ae1b3929926eb9
FuzuoZhang <zfz_ll@163.com>
2018-12-28 18:21:16 +0800

    update the source code

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def disorImg(image,blocksize):
 	# blocksize: the size of the block image: blocksize x blocksize
 	# blockimages: the image pieces from the original image
 
-    [w,h] = img.size
+    [w,h] = img.size 
     [colnum,rownum] = [math.floor(w/blocksize),math.floor(h/blocksize)]
     blocknum=colnum*rownum
 
@@ -44,9 +44,12 @@ def computeDijo(blockimages,blocksize, n):
 	for i in range(n):
 		B[:,:,i] = 1e-6*np.array(([[0,0,1],[1,1,1],[1,0,0]]))
 	for o in range(4):
-		bitmp = np.rot90(blockimages,o-1)
+		bitmp = np.zeros((blocksize,blocksize,3,n))
+		for t in range(n):
+			bitmp[:,:,:,t] = np.rot90(blockimages[:,:,:,t],o-1)
+		#bitmp = np.rot90(blockimages,o-1)
 		GiL = bitmp[:,blocksize-1,:,:] - bitmp[:,blocksize-2,:,:]
-		GiL = np.reshape(GiL,(blocksize,3,n))
+		#GiL = np.reshape(GiL,(blocksize,3,n))
 		GiL = np.vstack(([GiL,B]))
 		uiL = np.mean(GiL,0)
 		uiL = uiL.T
@@ -64,8 +67,9 @@ def computeDijo(blockimages,blocksize, n):
 				GijLR = bitmp[:,0,:,j] - bitmp[:,blocksize-1,:,i]
 				#GijLR = np.reshape(GijLR, (blocksize,3))
 				tmp = GijLR-uiL[i,:]
-				multmp= np.dot(tmp,invSiL[:,:,i])
-				Dijo[i,j,o] = np.trace(np.dot(multmp,tmp.T))   
+				#multmp= np.dot(tmp,invSiL[:,:,i])
+				#Dijo[i,j,o] = np.trace(np.dot(multmp,tmp.T))  
+				Dijo[i,j,o] = np.trace(tmp@invSiL[:,:,i]@tmp.T) 
 	return Dijo
 
 def computeWijo(Dijo, n):
@@ -167,7 +171,7 @@ def LPSolving(blocksize,blockimages,colnum,rownum):
 		for i in range(n):
 			for j in range(n):
 				for o in range(4):
-					if(abs(x[i]-x[j]-delox[o])>1e-5 or abs(y[i]-y[j]-deloy[o])>1e-5):
+					if(abs(x[i]-x[j]-delox[o])>1e-1 or abs(y[i]-y[j]-deloy[o])>1e-1):
 						U[i][j][o] = 0
 		[AA,A] = computeA(Dijo, U, n)
 
@@ -177,24 +181,27 @@ def LPRecover(blocksize,blockimages,colnum,rownum):
     n = blockimages.shape[3]
     [x, y] = LPSolving(blocksize,blockimages,colnum,rownum)
     recoverimg = np.zeros((rownum*blocksize,colnum*blocksize,3))
-    x = np.floor(x - min(x))
-    y = np.floor(y - min(y))
+    x = np.round(x - min(x))
+    y = np.round(y - min(y))
     x = x.astype(np.int)
     y = y.astype(np.int)
     for i in range(n):
+        #recoverimg[y[i]*blocksize:(y[i]+1)*blocksize,x[i]*blocksize:(x[i]+1)*blocksize,:] = blockimages[:,:,:,i]
         recoverimg[x[i]*blocksize:(x[i]+1)*blocksize,y[i]*blocksize:(y[i]+1)*blocksize,:] = blockimages[:,:,:,i]
     return recoverimg
 
 if __name__== '__main__':
 	#read a image
-	img = Image.open('lena.tif')
+	#img = Image.open('2.png')
+	img = Image.open('8.jpg')
+	#img = Image.open('lena.tif')
 	blocksize = 64
 	[disimage,blockimages,colnum,rownum] = disorImg(img,blocksize)
-	#[x,y] = LPSolving(blocksize,blockimages,colnum,rownum)
-	recoverimg = LPRecover(blocksize,blockimages,colnum,rownum)
 	plt.figure(1)
 	plt.imshow(disimage)
 	plt.show()
+	#[x,y] = LPSolving(blocksize,blockimages,colnum,rownum)
+	recoverimg = LPRecover(blocksize,blockimages,colnum,rownum)
 	plt.figure(2)
 	plt.imshow(recoverimg)
 	plt.show()
